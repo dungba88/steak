d3772b9ce22267a09a2139af27b48d1fb3f66b84
griever <griever@PC01NH3V.sg.oracle.com>
2016-09-14 17:33:38 +0800

    Add Apache license

--- src/main/java/org/joo/steak/framework/State.java
+++ src/main/java/org/joo/steak/framework/State.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.joo.steak.framework;
 
 import org.joo.steak.framework.event.StateChangedDispatcher;
@@ -24,8 +42,10 @@ public interface State extends StateChangedDispatcher {
 	
 	/**
 	 * Called by <code>StateManager</code> when exiting the current state
+	 * 
 	 * @param event
-	 * 			the original event dispatched by this state when changing state
+	 *            the original event dispatched by this state when changing
+	 *            state
 	 */
 	public void onExit(StateChangedEvent event);
 }
