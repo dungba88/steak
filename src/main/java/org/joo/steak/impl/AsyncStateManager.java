c7dc37f31bf98b65a5fce2ca3174a7eaaa30b66a
griever <griever@PC01NH3V.sg.oracle.com>
2016-09-19 16:53:06 +0800

    Add asynchronous implementation of StateManager

---
+++ src/main/java/org/joo/steak/impl/AsyncStateManager.java
@@ -0,0 +1,72 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.joo.steak.impl;
+
+import java.util.concurrent.ExecutorService;
+
+import org.joo.steak.framework.event.StateChangeEvent;
+
+/**
+ * Asynchronous implementation of <code>StateManager</code>. It relies on
+ * <code>ExecutorService</code> to do the job.
+ * 
+ * @author griever
+ *
+ */
+public class AsyncStateManager extends DefaultStateManager {
+
+	private ExecutorService service;
+
+	public AsyncStateManager(ExecutorService service) {
+		this.service = service;
+	}
+
+	@Override
+	public void doRun() {
+		service.execute(new Runnable() {
+
+			@Override
+			public void run() {
+				changeNextState(getInitialState(), null);
+			}
+		});
+	}
+
+	@Override
+	public void onStateChange(StateChangeEvent event) {
+		if (event == null)
+			return;
+
+		checkStateIntegrity(event);
+
+		String nextStateId = findNextState(event.getAction(), event.getArgs());
+
+		service.execute(new Runnable() {
+
+			@Override
+			public void run() {
+				changeNextState(nextStateId, event);
+			}
+		});
+	}
+
+	public ExecutorService getExecutorService() {
+		return service;
+	}
+}
