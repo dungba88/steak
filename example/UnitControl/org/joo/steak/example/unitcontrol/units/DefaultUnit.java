d8f403e1be6fa50e5eb94d2cd4f6e88c7bdb1473
griever <griever@PC01NH3V.sg.oracle.com>
2016-09-22 16:40:37 +0800

    Add lock

--- example/UnitControl/org/joo/steak/example/unitcontrol/units/DefaultUnit.java
+++ example/UnitControl/org/joo/steak/example/unitcontrol/units/DefaultUnit.java
@@ -32,93 +32,98 @@ import org.joo.steak.impl.DefaultStateManager;
 import org.joo.steak.impl.event.DefaultStateEngineListener;
 
 /**
- * Represent an unit. An unit can have HP, DMG, and its own state engine.
- * It can take turn and perform actions based on its current state.
+ * Represent an unit. An unit can have HP, DMG, and its own state engine. It can
+ * take turn and perform actions based on its current state.
  * 
  * @author griever
  *
  */
 public class DefaultUnit implements Unit {
-	
+
 	private double maxHP;
-	
+
 	private double hp;
-	
+
 	private double dmg;
 
 	private StateEngineConfiguration configuration;
-	
+
 	private StateManager stateManager;
-	
+
 	private String unitName;
+
+	private Object hpLock = new Object();
 
 	public DefaultUnit(String unitName, UnitType unitType, double hp, double dmg) {
 		this.unitName = unitName;
 		this.maxHP = hp;
 		this.hp = hp;
 		this.dmg = dmg;
-		
-		configuration = UnitConfigurationFactory.getConfigurator(unitType).getConfiguration();
-		
+
+		configuration = UnitConfigurationFactory.getConfigurator(unitType)
+				.getConfiguration();
+
 		stateManager = new DefaultStateManager();
 		StateContext stateContext = new DefaultStateContext("default");
 		stateContext.getContextMap().put("UNIT", this);
 		stateManager.initialize(stateContext, configuration, null);
 		stateManager.run();
-		
+
 		stateManager.registerExceptionHandler(new StateExceptionHandler() {
-			
+
 			@Override
 			public boolean handleException(StateExecutionException exception) {
 				System.out.println(exception.getCause().getMessage());
 				return true;
 			}
 		});
-		
+
 		stateManager.addStateEngineListener(new DefaultStateEngineListener() {
-			
+
 			@Override
 			public void onFinish(StateChangeEvent event) {
-				System.out.println(unitName + " finished! " + stateManager.getCurrentState() + " -> " + event.getAction());
+				System.out.println(unitName + " finished! "
+						+ stateManager.getCurrentState() + " -> "
+						+ event.getAction());
 			}
 		});
 	}
-	
+
 	@Override
 	public void changeTarget(Unit unit) {
 		if (this.equals(unit))
 			return;
-		
+
 		putContextMap("TARGET_UNIT", unit);
 	}
-	
+
 	@Override
 	public DefaultUnit getTargetUnit() {
 		return (DefaultUnit) getContextMap("TARGET_UNIT");
 	}
-	
+
 	@Override
 	public void performAction() {
 		if (isDead())
 			return;
-		
+
 		UnitState state = getCurrentState();
 		if (state != null)
 			state.performAction();
 	}
-	
+
 	@Override
 	public void onAttacked(Unit attackingUnit) {
 		putContextMap("ATTACKING_UNIT", attackingUnit);
-		
+
 		UnitState state = getCurrentState();
-		
+
 		if (state != null) {
 			ExecutorManager.getInstance().execute(new Runnable() {
-				
+
 				@Override
 				public void run() {
-						state.onAttacked(attackingUnit);
+					state.onAttacked(attackingUnit);
 				}
 			});
 		}
@@ -126,52 +131,57 @@ public class DefaultUnit implements Unit {
 
 	@Override
 	public void raiseHP(double hp) {
-		if (isDead())	// to make sure for concurrent issue
-			return;
-		this.hp += hp;
-		if (this.hp > maxHP)
-			this.hp = maxHP;
+		synchronized (hpLock) {
+			if (isDead()) // to make sure for concurrent issue
+				return;
+			this.hp += hp;
+			if (this.hp > maxHP)
+				this.hp = maxHP;
+		}
 	}
-	
+
 	@Override
 	public void reduceHP(double hp) {
-		this.hp -= hp;
-		if (this.hp < 0)
-			this.hp = 0;
+		synchronized (hpLock) {
+			this.hp -= hp;
+			if (this.hp < 0)
+				this.hp = 0;
+		}
 	}
-	
+
 	private void putContextMap(String key, Object value) {
 		stateManager.getStateContext().getContextMap().put(key, value);
 	}
-	
+
 	private Object getContextMap(String key) {
 		return stateManager.getStateContext().getContextMap().get(key);
 	}
-	
+
 	private UnitState getCurrentState() {
-		return (UnitState) stateManager.getState(stateManager.getCurrentState());
+		return (UnitState) stateManager
+				.getState(stateManager.getCurrentState());
 	}
-	
+
 	@Override
 	public double getHP() {
 		return hp;
 	}
-	
+
 	@Override
 	public double getMaxHP() {
 		return maxHP;
 	}
-	
+
 	@Override
 	public double getDamage() {
 		return dmg;
 	}
-	
+
 	@Override
 	public String getUnitName() {
 		return unitName;
 	}
-	
+
 	@Override
 	public boolean isDead() {
 		return hp == 0;
